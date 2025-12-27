708be9c167f6e76bae6946eb938e2d5c04d32605
KavyaJP <kavya31052005@gmail.com>
2025-12-27 10:35:59 +0530

    Added sound and finished the tutorial

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -6,5 +6,7 @@ var score: int = 0
 
 func add_point():
 	score += 1
-	print("Score: %d" % score)
-	score_label.text = "Congratulations! You collected %d coins!" % score
+	if(score == 10):
+		score_label.text = "Congratulations!\nYou collected all 10 coins!"
+	else:
+		score_label.text = "Congratulations!\nYou collected %d coins!" % score
